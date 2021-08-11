1ac9ae3591ebe62619d0a886e615dc5b8cc1e3a5
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-11 11:05:21 -0400

    Create spec file for Attendee

---
+++ spec/attendee_spec.rb
@@ -0,0 +1,9 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.describe Attendee do
+  it "exists and has attributes" do
+
+  end 
+end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,6 +5,7 @@ require './lib/attendee'
 RSpec.describe Item do
   it "exists and has attributes" do
     item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
 
     expect(item1).to be_a Item
     expect(item1.name).to eq("Chalkware Piggy Bank")
