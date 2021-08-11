d89d29de060918a338f659030b012098b08b540d
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-11 11:06:26 -0400

    Write test for attributes

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -4,6 +4,10 @@ require './lib/attendee'
 
 RSpec.describe Attendee do
   it "exists and has attributes" do
+    attendee = Attendee.new(name: 'Megan', budget: '$50')
 
-  end 
+    expect(attendee).to be_a Attendee
+    expect(attendee.name).to eq("Megan")
+    expect(attendee.budget).to eq(50)
+  end
 end
