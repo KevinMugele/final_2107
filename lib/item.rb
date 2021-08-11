eea572fa3ecebd4075935bf6e4930ccb088c9a4a
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-11 11:27:02 -0400

    Write method for current_high_bid

--- lib/item.rb
+++ lib/item.rb
@@ -10,4 +10,12 @@ class Item
   def add_bid(attendee, amount)
     @bids[attendee] += amount
   end
+
+  def current_high_bid
+    all_bids = []
+    @bids.each do |attendee, amount|
+      all_bids << amount
+    end
+    all_bids.max
+  end
 end
