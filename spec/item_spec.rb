5f208480324f0b38de36a31eb1733947a9963e09
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-11 11:03:40 -0400

    Write test for Item existing and having attributes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,5 +3,10 @@ require './lib/auction'
 require './lib/attendee'
 
 RSpec.describe Item do
+  it "exists and has attributes" do
+    item1 = Item.new('Chalkware Piggy Bank')
 
-end 
+    expect(item1).to be_a Item
+    expect(item1.name).to eq("Chalkware Piggy Bank")
+  end
+end
